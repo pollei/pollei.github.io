659bb354157e8d03a01a6ba676b096e8568f13ef
Stephen Pollei <stephen.pollei@gmail.com>
2023-04-15 16:28:26 -0700

    dynamic timesheet start
    move css into own file
    start on making timesheet dynamic
    modify sizes on some table cells

--- sfcc-cyber-bas/CYBR475/fun.js
+++ sfcc-cyber-bas/CYBR475/fun.js
@@ -4,7 +4,7 @@ function resetDialogForms(diaNd) {
 }
 function formToLclStore(formElem) {
     const inputElements = formElem.querySelectorAll('[name]')
-    for (inpEl of inputElements) {
+    for (let inpEl of inputElements) {
         //console.log(inpEl)
         //if ('text' == inpEl.type || ('date' == inpEl.type)) {
             localStorage.setItem('sfcc::cybr475::intern-data::' + inpEl.name, '' + inpEl.value)
@@ -14,7 +14,7 @@ function formToLclStore(formElem) {
 }
 function lclStoreToForm(formElem) {
     const inputElements = formElem.querySelectorAll('[name]')
-    for (inpEl of inputElements) {
+    for (let inpEl of inputElements) {
         const dynVal = localStorage.getItem('sfcc::cybr475::intern-data::' +inpEl.name)
         if (null != dynVal && dynVal.length > 0) {
             inpEl.value = dynVal
@@ -23,7 +23,7 @@ function lclStoreToForm(formElem) {
 }
 function lclStoreToDocument() {
     const dynamicElems = document.querySelectorAll('[data-intern-store]')
-    for (dynEl of dynamicElems) {
+    for (let dynEl of dynamicElems) {
         const dynVal = localStorage.getItem('sfcc::cybr475::intern-data::' + dynEl.dataset.internStore)
         //console.log(dynEl, dynVal)
         if (null != dynVal && dynVal.length > 0) {
@@ -51,3 +51,72 @@ function dalogClose_cb(ev) {
     }
     
 }
+function nukeButt_cb(evt) {
+    let keys = []; 
+    evt.stopPropagation();
+    evt.preventDefault();
+    for (let i=0; i<localStorage.length; i++) {
+        if (localStorage.key(i).match(/^sfcc::cybr475::intern-data::/)) {
+            keys.push(localStorage.key(i)); }
+    }
+    //console.log(keys);
+    keys.forEach( x => localStorage.removeItem(x));
+    return;
+}
+
+function is_val_date_str(dts ) {
+    if (!dts.match(/\d\d\d\d-\d\d-\d\d/)) return false;
+    let dta = dts.split('-',3).map( x => parseInt(x,10));
+    //console.log(dta);
+    if (dta[0]<2021 || dta[0]>2025) {return false;}
+    if (dta[1]<1 || dta[1]>12) {return false;}
+    if (dta[1]<1 || dta[1]>31) {return false;}
+    let dt = new Date(dta[0],dta[1]-1, dta[2], 2 );
+    // gawd the month is zero indexed so jan is 0, dec is 11
+    // b0rken
+    //console.log(dt);
+    return true;
+}
+function str_to_Date(dts) {
+    let dta = dts.split('-',3).map( x => parseInt(x,10));
+    return new Date(dta[0],dta[1]-1, dta[2] );
+}
+function Date_to_str(dt) {
+    function df2(n) { if (n<10) return '0' + n.toString(10); return n.toString(10);}
+    return [dt.getFullYear(), dt.getMonth()+1, dt.getDate() ].map(df2).join('-');
+}
+function Date_within_days_now(dts, n) {
+    let bnchMrk = str_to_Date(dts).getTime() + (+n *86400000);
+    return Date.now() < bnchMrk;
+}
+function getDateOffs(dts) {
+    return Math.floor((Date.now() - str_to_Date(dts).getTime()) / 86400000 );
+}
+function getTmplCloneSrc(tmplId, itmClass) {
+    const tmpl= document.getElementById(tmplId);
+    //console.log(tmpl, tmpl.content)
+    for (let elem of tmpl.content.childNodes) {
+        //console.log(elem);
+        if (elem.classList?.contains(itmClass)) return elem;
+    }
+    return null;
+}
+function tmplClone(tmplId, itmClass) {
+    //const srcElem = getTmplCloneSrc(tmplId, itmClass) ;
+    //console.log(srcElem);
+    return getTmplCloneSrc(tmplId, itmClass)?.cloneNode(true);
+}
+function addEventListenerById(nodeId, eventName, callback) {
+    let node = document.getElementById(nodeId);
+    node.addEventListener(eventName, callback);
+}
+function initTimeSheet0() {
+    const timeSheetAutoPages = document.getElementById('timeSheetAutoPages');
+    let tsPage = tmplClone("tmpl-timeSheetAutoPages","timeSheetPage");
+    let tsFooter = tmplClone("tmpl-timeSheetAutoPages","timeSheetFinalFooter");
+    timeSheetAutoPages.appendChild(tsPage);
+    tsPage.appendChild(tsFooter);
+}
+
+/*
+ * https://pollei.github.io/sfcc-cyber-bas/ASTR100/wk-sched/?strtDay=2023-03-31
